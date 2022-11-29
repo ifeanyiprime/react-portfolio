a6f3cd142fb248d8e3870a0a68e0307b96c7444d
ifeanyiprime <ifeanyi20162016@outlook.com>
2022-11-29 17:59:41 +0100

    added a body

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube} from 'react-icons/ai'
+import Image from 'next/image'
+import deved from '../public/dev-ed-wave.png'
+import design from '../public/design.png'
+import code from '../public/code.png'
+import consulting from '../public/consulting.png'
 
 export default function Home() {
   return (
@@ -14,6 +19,7 @@ export default function Home() {
 
       <main className="bg-white px-10">
         <section className=" min-h-screen">
+
           <nav className="py-10 mb-12 flex justify-between">
             <h1 className=" font-burtons text-xl">Ifeanyi Patrick</h1>
             <ul className="flex items-center">
@@ -23,13 +29,42 @@ export default function Home() {
               <li><a className=" bg-gradient-to-r  from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="">Resume</a></li>
             </ul>
           </nav>
-          <div>
-            <h2>Ifeanyi E. Patrick</h2>
-            <h3>Developer</h3>
-            <p>
+
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">Ifeanyi E. Patrick</h2>
+            <h3 className="text-2xl py-2">Developer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">
               Freelancer providing services for programming and design content needs. Join me below and let's get
               cracking!
             </p>
+          </div>
+
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillTwitterCircle/>
+            <AiFillLinkedin/>
+            <AiFillYoutube/>
+            
+          </div>
+
+          <div className=" relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+              <Image src={deved} layout="fill" objectFit="cover" />
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Since the beginning of my journey as a Freelancer designer and developer I've done remote work for 
+              <span className="text-teal-500"> agencies </span> consulted for <span className="text-teal-500"> startups </span> 
+              and colaborated with talented people to create digital for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              I offer from a wide range of services, including brand design, programming and teaching.
+            </p>
+          </div>
+          <div>
+            
           </div>
         </section>
       </main>
