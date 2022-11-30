c16ce76df5f3fd074598cca7e767a234320a3146
ifeanyiprime <ifeanyi20162016@outlook.com>
2022-11-30 11:58:03 +0100

    wrapping up with styling

--- pages/components/card.js
+++ pages/components/card.js
@@ -1,17 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
 function Card(props) {
-    return (
-        <div className="text-center shadow-lg p-10 rounded-xl my-10">
-              <Image className="mx-auto" src={props.src} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
-              <p className="py-2">
-                {props.body}
-              </p>
-              <h4 className="py-1 text-teal-600">{props.sub}</h4>
-              {props.subBody.map(cont => {
-                return <p className="text-gray-800 py-1">{cont}</p>
-              })}
-        </div>
-    )
+  return (
+    <div className="text-center shadow-lg p-10 rounded-xl my-10">
+      <Image className="mx-auto" src={props.src} width={100} height={100} />
+      <h3 className="text-lg font-semibold pt-8 pb-2">{props.head}</h3>
+      <p className="py-2">{props.body}</p>
+      <h4 className="py-1 text-teal-600">{props.sub}</h4>
+      {props.subBody.map((cont) => {
+        return <p className="text-gray-800 py-1">{cont}</p>;
+      })}
+    </div>
+  );
 }
+
+export default Card;
